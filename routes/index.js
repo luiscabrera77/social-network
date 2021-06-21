a317796e7bce73373ceed22e102e72645797401a
Luis <luis.cabrera@gmail.com>
2021-06-21 02:16:45 -0400

    cleaned code

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.use('/api', apiRoutes);
 
 // 404 Status error message
 router.use((req, res) => {
-    res.status(404).send('<h1>404 Error....</h1>');
+    res.status(404).send('404 Error');
   });
 
 // Module exports router
